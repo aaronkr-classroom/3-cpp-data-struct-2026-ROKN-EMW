beb040d3260aabe2c8433923047f6668b3d5c728
ROKN-EMW <seoseo471@gmail.com>
2026-03-22 11:47:16 +0900

    Homework

---
+++ Week3/3-4.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+
+    cout << "단어를 입력하세요:  ";
+
+    vector<string> words;
+    string input;
+
+    // 문자열 계속 입력
+    while (cin >> input) {
+        words.push_back(input);
+    }
+
+    // 입력 확인
+    if (words.size() == 0) {
+        cout << "Error: No words entered!" << endl;
+        return 1;
+    }
+
+    // 가장 긴 / 짧은 길이 찾기
+    int longstr = words[0].length();
+    int shortstr = words[0].length();
+
+    for (int i = 0; i < words.size(); i++) {
+        int len = words[i].length();
+
+        if (len > longstr)
+            longstr = len;
+
+        if (len < shortstr)
+            shortstr = len;
+    }
+
+    cout << "Longest length : " << longstr << endl;
+    cout << "Shortest length: " << shortstr << endl;
+
+    return 0;
+}
