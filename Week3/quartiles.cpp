42b979830b62ae60b3aab69e7f59b83dd02e8e62
ththfql <145094239+ththfql@users.noreply.github.com>
2026-03-25 15:24:03 +0900

    Example commit

---
+++ Week3/quartiles.cpp
@@ -0,0 +1,55 @@
+﻿//quartiles.cpp
+
+/*
+	25% => 출력
+	50%(중앙값) => 출력
+	75% => 출력
+*/
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+typedef vector<int>::size_type vec_sz;
+
+
+int main(void)
+{ 
+	// 프로그램 설명
+	cout << "Enter all values, then EOF: ";
+
+	vector<int> val;
+	int in;
+	
+	while (cin >> in) {
+		val.push_back(in);
+
+	}
+
+	// 오류 체킹
+	vec_sz size = val.size();
+	if (size == 0) {
+		cout << "No values. Try again!" << endl;
+
+		return 1;
+	}
+	// 정렬
+	sort(val.begin(), val.end());
+
+	vec_sz q1 = size / 4; //제 1 사분위수 (25%)
+	vec_sz q2 = size / 2; //제 2 사분위수 (50%)
+	vec_sz q3 = size / 4 * 3; //제 3 사분위수 (75%)
+
+	int q1_val, q2_val, q3_val;
+	q1_val = size % 4 == 0 ? (val[q1] + val[q1 - 1]) / 2 : val[q1];
+	q2_val = size % 2 == 0 ? (val[q2] + val[q2 - 1]) / 2 : val[q2];
+	q3_val = 3 * size % 4 == 0 ? (val[q3] + val[q3 - 1]) / 2 : val[q3];
+
+	cout <<
+		"Q1: " << q1 << endl <<
+		"Q2 (중앙값): " << q2 << endl <<
+		"Q3: " << q3 << endl;
+
+	return 0;
+}
---
+++ Week3/str_size.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void)
+{
+	cout << "Enter all words, then EOF: ";
+
+	//입력받기
+	string word;
+	string max = "";
+	string min = "";
+
+	while (cin >> word) {
+		if (min.size() == 0) min = word; //초기화
+		if (max.size() == 0) max = word;
+
+		//크기 확인
+		if (word.size() < min.size()) min = word;
+		else if (word.size() > max.size()) max = word;
+
+	}
+	// 결과 출력
+	cout <<
+		"Min word: " << min << "size: " << min.size() << endl <<
+		"Max word: " << max << "size: " << max.size() << endl;
+
+
+	return 0;
+}
